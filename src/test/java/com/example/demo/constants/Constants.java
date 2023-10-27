31b719c3557370b3b5b540d01f36dece31c25b8e
Bruno Sa <brunosantana.engenharia@gmail.com>
2023-10-27 11:03:39 -0300

    testes do dataprovider findById

--- src/test/java/com/example/demo/constants/Constants.java
+++ src/test/java/com/example/demo/constants/Constants.java
@@ -4,10 +4,17 @@ import com.example.demo.app.dto.Response;
 import com.example.demo.domain.model.ProductModel;
 import com.example.demo.infra.feign.ProdutoDto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Constants {
-    private static final Response RESPONSE = new Response();
 
-    private static final ProductModel PRODUCT_MODEL = new ProductModel();
+    public static final List<String> image = new ArrayList<>();
+    public static final Response RESPONSE = new Response(1L,"Gulab Powder 50 Gram","Dry Rose Flower Powder Gulab Powder 50 Gram • Treats Wounds", 70L,13L,4L,47L,"Dry Rose","groceries", "https://i.dummyjson.com/data/products/25/thumbnail.jpg",image );
 
-    private static final ProdutoDto PRODUTO_DTO = new ProdutoDto();
+    public static final List<ProductModel> PRODUCT_MODEL_LIST = new ArrayList<>();
+    public static final ProductModel PRODUCT_MODEL = new ProductModel(1L,"Gulab Powder 50 Gram","Dry Rose Flower Powder Gulab Powder 50 Gram • Treats Wounds", 70L,13L,4L,47L,"Dry Rose","groceries", "https://i.dummyjson.com/data/products/25/thumbnail.jpg",image);
+
+
+    public static final ProdutoDto PRODUTO_DTO = new ProdutoDto(PRODUCT_MODEL_LIST,10L,5L,5L);
 }
--- src/test/java/com/example/demo/infra/DataProviderTest.java
+++ src/test/java/com/example/demo/infra/DataProviderTest.java
@@ -1,4 +1,59 @@
 package com.example.demo.infra;
 
+import com.example.demo.constants.Constants;
+import com.example.demo.domain.model.ProductModel;
+import com.example.demo.infra.dataprovider.DataProviderProduto;
+import com.example.demo.infra.feign.FeingClient;
+import feign.FeignException;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityNotFoundException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static com.example.demo.constants.Constants.*;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class DataProviderTest {
+
+    @Mock
+    private FeingClient  feingClient;
+
+    @InjectMocks
+    private DataProviderProduto dataProviderProduto;
+
+    @Test
+    public void findById(){
+
+        when(feingClient.findById(1L)).thenReturn((PRODUCT_MODEL));
+
+        ProductModel sut = dataProviderProduto.buscarPorIdGateway(1l);
+
+        Assertions.assertThat(sut).isNotNull();
+        Assertions.assertThat(sut).isEqualTo(PRODUCT_MODEL);
+
+    }
+
+    @Test
+    public void findById_NotFound(){
+
+        when(feingClient.findById(anyLong())).thenThrow(FeignException.NotFound.class);
+
+        dataProviderProduto.buscarPorIdGateway(1L);
+
+        Assertions.assertThatExceptionOfType(EntityNotFoundException.class).isThrownBy(()->dataProviderProduto.buscarPorIdGateway(1L));
+
+    }
+
+
+
 }
