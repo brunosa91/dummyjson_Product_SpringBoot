8fd3df26dbcd6c5be865d694765ff71e3bea811f
Bruno Sa <brunosantana.engenharia@gmail.com>
2023-10-27 14:22:06 -0300

    teste na controller de findByid e findaAll realizados

--- src/main/java/com/example/demo/app/controller/ControllerProduto.java
+++ src/main/java/com/example/demo/app/controller/ControllerProduto.java
@@ -19,8 +19,6 @@ public class ControllerProduto {
     @Autowired
     ServiceProduct serviceProduct;
 
-    @Autowired
-    FeingClient  feingClient;
 
     @GetMapping(value = "/{id}")
  public ResponseEntity<Response> buscaPorId(@PathVariable Long id){
--- src/test/java/com/example/demo/app/ControllerTest.java
+++ src/test/java/com/example/demo/app/ControllerTest.java
@@ -1,19 +1,26 @@
 package com.example.demo.app;
 
 import com.example.demo.app.controller.ControllerProduto;
+import com.example.demo.app.dto.Response;
 import com.example.demo.app.mapper.MapperServiceImpl;
 import com.example.demo.app.service.ServiceProduct;
 import com.example.demo.app.service.ServiceProductImpl;
 import com.example.demo.constants.Constants;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static com.example.demo.constants.Constants.*;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,7 +44,46 @@ public class ControllerTest {
 
     @Test
     public void findProductById_ReturnProduct() throws Exception{
-    //when(serviceProduct.findById(1L)).thenReturn(PRODUCT_MODEL);
+    when(serviceProduct.findById(1L)).thenReturn(RESPONSE);
+
+
+    mockMvc.perform(get("/produto/1"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value(RESPONSE.getId()));
     }
 
+    @Test
+    public void findProductByUnexistingId_ReturnNotFound() throws Exception{
+        when(serviceProduct.findById(1L)).thenThrow(EntityNotFoundException.class);
+
+
+        mockMvc.perform(get("/produto/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void findAll_ReturnProductList() throws Exception{
+
+        List<Response> responseList = new ArrayList<>();
+        responseList.add(RESPONSE);
+        when(serviceProduct.findAll()).thenReturn(responseList);
+
+
+        mockMvc.perform(get("/produto"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(1)))
+                .andExpect(jsonPath("$[0].id").value(RESPONSE.getId()));
+    }
+
+    @Test
+    public void findAll_ReturnEmptyList() throws Exception{
+        when(serviceProduct.findAll()).thenReturn(Collections.emptyList());
+
+
+        mockMvc.perform(get("/produto"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(0)));
+    }
+
+    
 }
